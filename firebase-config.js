c14a2710e58fb7e9f46486b8beb4eda66164abcf
trippy-ezra <ezramaritim08@gmail.com>
2023-06-21 13:36:37 +0300

    check

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAFOUwcLsLkZlhWXFA8lnNFuUrlIAVQ2Is",
-  authDomain: "pg-extension-test.firebaseapp.com",
-  projectId: "pg-extension-test",
-  storageBucket: "pg-extension-test.appspot.com",
-  messagingSenderId: "156122766890",
-  appId: "1:156122766890:web:71b484d59278a3a5fead53",
-  measurementId: "G-DDR0S5GGN5"
+  apiKey: "AIzaSyBVXDQVIiL7pU5UXAhbBH_Yah3w1V910oU",
+  authDomain: "pgpanda--auth.firebaseapp.com",
+  databaseURL: "https://pgpanda--auth-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pgpanda--auth",
+  storageBucket: "pgpanda--auth.appspot.com",
+  messagingSenderId: "127198853640",
+  appId: "1:127198853640:web:622c23dfb286ece3f76fbd"
   };
   
   
