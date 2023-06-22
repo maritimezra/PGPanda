b74e304eba00ad21145f7ac21f63557d892795cb
trippy-ezra <ezramaritim08@gmail.com>
2023-06-22 12:49:39 +0300

    oauth2

--- blocked.js
+++ blocked.js
@@ -3,19 +3,25 @@ document.getElementById('safety-button').addEventListener('click', function(){
     window.close();
 });
 // Add a click event listener to the "override-button"
+  
 document.getElementById('override-button').addEventListener('click', function() {
-    // Open Firebase Google sign-in pop-up
-    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
-      .then(function(result) {
-        // User signed in successfully
-        chrome.tabs.create({ url: 'configuration.html' });
-        window.close();
-      })
-      .catch(function(error) {
-        // Handle sign-in errors
-        console.error('Sign-in error:', error);
-        console.error(error.message);
-      });
+  // Clear user's authentication state
+  firebase.auth().signOut().then(function() {
+    // User signed out successfully
+    // Request Google sign-in
+    chrome.identity.getAuthToken({ interactive: true }, function(token) {
+      if (chrome.runtime.lastError) {
+        console.error('Sign-in error:', chrome.runtime.lastError);
+        return;
+      }
+  
+      // User has signed in, proceed to the configuration page
+      chrome.tabs.create({ url: 'configuration.html' });
+      window.close();
+    });
+  }).catch(function(error) {
+    console.error('Sign-out error:', error);
   });
+});
   
      
