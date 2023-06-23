43852b4f1960dd16e17fd2af5406aef78aebbbf8
trippy-ezra <ezramaritim08@gmail.com>
2023-06-23 18:01:18 +0300

    email/password auth

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,5 @@
+//import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBVXDQVIiL7pU5UXAhbBH_Yah3w1V910oU",
   authDomain: "pgpanda--auth.firebaseapp.com",
@@ -14,4 +16,66 @@ const firebaseConfig = {
   catch(e){
     console.log("Firebase not initialized", e);
   }
+
+  // Rest of your code...
+
+function registerUser() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function(userCredential) {
+      // User registration successful
+      var user = userCredential.user;
+      console.log('Registered user:', user);
+      //alert("Registration Successful!");
+      chrome.tabs.create({ url: 'login.html' });
+      window.close();
+    })
+    .catch(function(error) {
+      // Handle registration errors
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.error('Registration error:', errorCode, errorMessage);
+    });
+}
+
+function loginUser() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(userCredential) {
+      // User login successful
+      var user = userCredential.user;
+      console.log('Logged in user:', user);
+      //alert("Log in successful")
+      chrome.tabs.create({ url: 'configuration.html' });
+      window.close();
+    })
+    .catch(function(error) {
+      // Handle login errors
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.error('Login error:', errorCode, errorMessage);
+    });
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+  var signupButton = document.getElementById('signup-button');
+  if (signupButton) {
+    signupButton.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevents the form from submitting normally
+      registerUser();
+    });
+  }
+
+  var loginButton = document.getElementById('login-button');
+  if (loginButton) {
+    loginButton.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevents the form from submitting normally
+      loginUser();
+    });
+  }
+});
   
