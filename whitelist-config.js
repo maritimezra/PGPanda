6488d02ceb461488419be7403b75f9f2d80e278d
trippy-ezra <ezramaritim08@gmail.com>
2023-07-12 23:52:51 +0300

    whitelist config

---
+++ whitelist-config.js
@@ -0,0 +1,60 @@
+document.getElementById('save-button').addEventListener('click', function(){
+    chrome.tabs.create({ url: 'chrome://newtab' });
+    window.close();
+});
+
+
+chrome.storage.sync.get(['whiteList'], function(result) {
+    var whiteList = result.whiteList;
+    if (whiteList && whiteList.length > 0) {
+      var whiteListSites = document.getElementById('whitelisted-sites-list');
+      whiteList.forEach(function(site) {
+        var listItem = document.createElement('li');
+        listItem.classList.add('site-item');
+  
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.name = 'whitelist-site';
+        checkbox.value = site;
+  
+        var label = document.createElement('label');
+        label.textContent = site;
+  
+        listItem.appendChild(checkbox);
+        listItem.appendChild(label);
+  
+        whiteListSites.appendChild(listItem);
+      });
+    }
+  });
+  
+  document.getElementById('add-button').addEventListener('click', function() {
+    var site = prompt('Enter the website URL to block:');
+    if (site && site.trim() !== '') {
+      var listItem = document.createElement('li');
+      var checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.name = 'whitelist-site';
+      checkbox.value = site;
+      listItem.appendChild(checkbox);
+
+       var link = document.createElement('a');
+            link.href = site;
+            link.target = '_blank';
+            link.textContent = site;
+            listItem.appendChild(link);
+
+      var whiteListSites = document.getElementById('whitelisted-sites-list');
+      whiteListSites.appendChild(listItem);
+    }
+});
+
+document.getElementById('remove-button').addEventListener('click', function() {
+    var whitelistedSitesList = document.getElementById('whitelisted-sites-list');
+    var checkedItems = whitelistedSitesList.querySelectorAll('input[type="checkbox"]:checked');
+    checkedItems.forEach(function(checkbox) {
+        var listItem = checkbox.parentNode;
+        whitelistedSitesList.removeChild(listItem);
+    });
+});
+
--- whitelist.js
+++ whitelist.js
@@ -1,59 +1,27 @@
-document.getElementById('proceed-button').addEventListener('click', function(){
-    chrome.tabs.create({ url: 'chrome://newtab' });
-    window.close();
-});
-
-
 chrome.storage.sync.get(['whiteList'], function(result) {
     var whiteList = result.whiteList;
     if (whiteList && whiteList.length > 0) {
       var whiteListSites = document.getElementById('whitelisted-sites-list');
       whiteList.forEach(function(site) {
         var listItem = document.createElement('li');
-        var checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.name = 'whitelist-site';
-            checkbox.value = site;
-            listItem.appendChild(checkbox);
-
-            var link = document.createElement('a');
-            link.href = site;
-            link.target = '_blank';
-            link.textContent = site;
-            listItem.appendChild(link);
-
-            whiteListSites.appendChild(listItem);
+        
+        var link = document.createElement('a');
+        link.href = site;
+        link.target = '_blank';
+        link.textContent = site;
+        
+        listItem.appendChild(link);
+        whiteListSites.appendChild(listItem);
       });
     }
-    
   });
-  document.getElementById('add-button').addEventListener('click', function() {
-    var site = prompt('Enter the website URL to block:');
-    if (site && site.trim() !== '') {
-      var listItem = document.createElement('li');
-      var checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.name = 'whitelist-site';
-      checkbox.value = site;
-      listItem.appendChild(checkbox);
 
-       var link = document.createElement('a');
-            link.href = site;
-            link.target = '_blank';
-            link.textContent = site;
-            listItem.appendChild(link);
-
-      var whiteListSites = document.getElementById('whitelisted-sites-list');
-      whiteListSites.appendChild(listItem);
-    }
+document.getElementById('configure-button').addEventListener('click', function() {
+    chrome.tabs.create({ url: 'whitelist-config.html' });
+    window.close();
 });
 
-document.getElementById('remove-button').addEventListener('click', function() {
-    var whitelistedSitesList = document.getElementById('whitelisted-sites-list');
-    var checkedItems = whitelistedSitesList.querySelectorAll('input[type="checkbox"]:checked');
-    checkedItems.forEach(function(checkbox) {
-        var listItem = checkbox.parentNode;
-        whitelistedSitesList.removeChild(listItem);
-    });
+document.getElementById('close-button').addEventListener('click', function() {
+    chrome.tabs.create({ url: 'chrome://newtab' });
+    window.close();
 });
-
